5b8ba2a5cfddf10d70c9c24e84043c41ed60b52a
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2024-11-05 11:03:02 +0530

    debugged some errors

--- src/main/java/com/zenveus/the_culinary_academy/config/FactoryConfiguration.java
+++ src/main/java/com/zenveus/the_culinary_academy/config/FactoryConfiguration.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.io.IOException;
 import java.util.Properties;
 
 public class FactoryConfiguration {
